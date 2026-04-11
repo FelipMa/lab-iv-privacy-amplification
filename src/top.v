05331d31324cbb9ce381820a05812bd879f8e5f8
FelipMa <felipeapenburg@gmail.com>
2026-04-11 11:25:46 -0300

    test top module with valid params

--- src/top.v
+++ src/top.v
@@ -1,20 +1,20 @@
 module top #(
-    // Alterar parâmetros aqui para ver o impacto na síntese
-    parameter P = 32,
-    parameter W = 32
+    parameter P = 1042,
+    parameter W = 64
 )(
     input  wire clk,
     input  wire reset,
     
-    // Entradas do tamanho exato para nao sofrerem otimizacao
-    input  wire [W-1:0] key,
-    input  wire [(W+P-2):0] matrix_window,
-    
-    // A saida precisa ir para um pino externo para nao ser removida
-    output wire [P-1:0] out
+    // Um pino de saída real
+    output wire dummy_out 
 );
 
-    // Instanciacao da Compression Unit
+    // Registradores internos simulando a chegada de dados
+    reg [W-1:0] key;
+    reg [(W+P-2):0] matrix_window;
+    
+    wire [P-1:0] hash_out;
+
     compression_unit #(
         .P(P),
         .W(W)
@@ -23,7 +23,24 @@ module top #(
         .reset         (reset),
         .key           (key),
         .matrix_window (matrix_window),
-        .hash_out      (out)
+        .hash_out      (hash_out)
     );
+
+    // Lógica para variar as entradas a cada clock
+    always @(posedge clk) begin
+        if (reset) begin
+            key <= {W{1'b1}}; 
+            matrix_window <= 0;
+        end else begin
+            // Varia a chave e a matriz continuamente
+            key <= ~key;
+            matrix_window <= matrix_window + 1'b1;
+        end
+    end
+
+    // Reduz todos os bits do hash em 1 único bit.
+    // Isso OBRIGA o sintetizador a calcular toda a matriz de P e W
+    // para conseguir saber qual será o valor lógico deste único pino.
+    assign dummy_out = ^hash_out;
 
 endmodule
